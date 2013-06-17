8513fa415b9c5162b6ad12dbe8afbed3fe114a4f
Mike <gostev@ebi.ac.uk>
2013-06-17 11:48:17 +0100

    Continue with bios

--- src/ruslo2013.asm
+++ src/ruslo2013.asm
@@ -1,5 +1,15 @@
-.EQU P1STAT  #021H
-.EQU KEYTBL  #030H
+.EQU P1STAT    #021H
+.EQU LASTKEY   #022H
+.EQU REPDLYCNT #023H
+.EQU REPPSECNT #024H
+.EQU REPDELAY  #025H
+.EQU REPPAUSE  #026H
+.EQU FLAGS     #027H ; b0 - передача разрешена
+.EQU PULSE     #028H
+
+.EQU CLCKDLY   2
+
+.EQU KEYTBL    #030H
 
 .org 0
 
@@ -109,11 +119,54 @@ anl     p1,#0EFH
 
 mov	a,r7
 andl	a,r6
-jnz	update_counter ; эта клавиша уже была нажата
+jz	new_key_pressed ; эта клавиша не была нажата
+
+mov	r0,LASTKEY
+mov	a,@r0
+xrl	a,r2 ; проверяем, была ли эта клавиша нажата последней
+jnz	update_counter ; если это не последняя клавиша, то ничего не делаем
+
+inc	r0  ; теперь r0 == REPDLYCNT
+mov	a,@r0
+jz	do_repeat
+dec	a
+mov	@r0,a
+jmp	update_counter ; просто уменьшаем счётчик задержки и сваливаем
+
+do_repeat:
+
+inc	r0  ; теперь r0 == REPPSECNT
+mov	a,@r0
+jz	send_repeat
+dec	a
+mov	@r0,a
+jmp	update_counter ; просто уменьшаем счётчик интервала и сваливаем
+
+send_repeat:
+mov	a,REPPAUSE
+mov	@r0,a ; инициализируем счётчик интервала репитера
+jmp	send_key
+
+new_key_pressed:
 
 mov	a,r7
 orl	a,r6
 mov	@r0,a
+
+mov	r0,LASTKEY ; сохраняем последнюю нажатую клавишу. Нужно для репитера
+mov	a,r2
+mov	@r0,a
+
+inc	r0 ; теперь r0 == REPDLYCNT
+mov	a,REPDELAY
+mov	@r0,a ; инициализируем счётчик задержки репитера
+
+inc	r0 ; теперь r0 == REPPSECNT
+mov	a,REPPAUSE
+mov	@r0,a ; инициализируем счётчик интервала репитера
+
+
+send_key:
 
 mov	r6,r2
 call	send_r6
@@ -197,7 +250,7 @@ retr
 ;-----------------------------------
 Beep:
 mov     r6,#0FEH
-mov     r1,#021H
+mov     r1,P1STAT
 
 beep_loop:
 mov     a,@r1
@@ -209,6 +262,81 @@ call    Delay
 mov     @r1,a
 djnz    r6,beep_loop
 
+retr
+
+;------------------------------------
+
+send_r6:
+mov	r0,FLAGS
+mov	a,@r0
+jb0	end_of_send ; передача не разрешена
+
+mov	r0,PUSLE
+mov	a,@r0
+mov	r4,a
+mov	r5,#001H
+
+mov	r1,P1STAT
+mov	a,@r1
+
+anl	a,#07FH
+mov	@r1,a
+outl	p1,a     ; start bit
+
+mov	r3,0 ; счётчик чётности
+
+anl	a,#03FH  ; clock pulse
+outl	p1,a
+
+mov	r7,#008H
+
+send_loop:
+
+call	Delay
+orl	p1,#040H
+
+mov	a,r6
+anl	a,#001H
+add	a,r3
+mov	r3,a
+
+mov	a,r6
+rr	a
+mov	r6,a
+anl	a,#080H
+add	a,@r1
+outl	p1,a
+
+call	Delay
+anl	p1,#040H
+
+djnz	r7,send_loop
+
+call	Delay
+orl	p1,#040H
+
+mov	a,r3 ; посылаем бит чётности
+rr	a
+anl	a,#080H
+add	a,@r1
+outl	p1,a
+
+call	Delay
+anl	p1,#040H
+
+call	Delay
+orl	p1,#040H
+
+anl	p1,#080H ; стоповый бит
+
+call	Delay
+anl	p1,#040H
+
+call	Delay
+orl	p1,#040H
+
+
+end_of_send:
 retr
 
 ;------------------------------------
